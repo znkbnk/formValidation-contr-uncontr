0a6010e12490280a61effefc235db882cd4113db
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-06-24 19:47:48 +0100

    Create Form.js

---
+++ Form.js
@@ -0,0 +1,103 @@
+//Form.js
+
+import React, { useState } from 'react';
+
+const Form = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const validateForm = () => {
+    let isValid = true;
+
+    if (name.trim() === '') {
+      setNameError('Name is required');
+      isValid = false;
+    } else {
+      setNameError('');
+    }
+
+    if (email.trim() === '') {
+      setEmailError('Email is required');
+      isValid = false;
+    } else if (!isValidEmail(email)) {
+      setEmailError('Invalid email');
+      isValid = false;
+    } else {
+      setEmailError('');
+    }
+
+    if (password.trim() === '') {
+      setPasswordError('Password is required');
+      isValid = false;
+    } else {
+      setPasswordError('');
+    }
+
+    return isValid;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (validateForm()) {
+      // Perform form submission logic here
+      console.log('Form submitted successfully!');
+    }
+  };
+
+  const isValidEmail = (email) => {
+    // Email validation logic (e.g., regex or other checks)
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        {nameError && <p className="error">{nameError}</p>}
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {emailError && <p className="error">{emailError}</p>}
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {passwordError && <p className="error">{passwordError}</p>}
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Form Validation</h1>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
